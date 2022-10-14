4e1b58f265ba528ca901dba2980581a20ee5a48c
Goph <tealeverywhere@gmail.com>
2022-10-13 19:07:02 -1000

    made tab0

--- src/index.js
+++ src/index.js
@@ -15,3 +15,43 @@ for(let i = 0; i < 3; i++){
   tab.innerText = `tab${i}`;
   tabContainer.appendChild(tab);
 }
+
+
+//make tab1
+function makeTab1(){
+  let mainDiv = document.createElement("div");
+  mainDiv.id = "mainDiv";
+  mainDiv.style.gridRowStart = '2';
+  mainDiv.style.gridColumn= '1/4';
+  mainDiv.style.border = "2px solid black";
+  mainDiv.style.display = "grid";
+  mainDiv.style.width = '65%';
+  mainDiv.style.justifyItems = "center";
+  mainDiv.style.gridTemplateRows = "1fr 2fr 2fr";
+
+  let titleDiv = document.createElement("div");
+  titleDiv.innerHTML = "Welcome to Food";
+  titleDiv.className = "titleDiv";
+  titleDiv.style.fontSize = "30px";
+  titleDiv.style.fontFamily = "sans-serif";
+
+  let aboutDiv = document.createElement("div")
+  let aboutSubDiv1 = document.createElement("div")
+  aboutSubDiv1.innerHTML = "About This Restaurant"
+  aboutSubDiv1.style.display = "grid";
+  aboutSubDiv1.style.justifyItems = "center";
+  aboutSubDiv1.style.fontFamily = "sans-serif";
+  aboutSubDiv1.style.fontSize = "20px";
+  aboutDiv.appendChild(aboutSubDiv1);
+
+  let aboutSubDiv2 = document.createElement("div");
+  aboutSubDiv2.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  aboutDiv.appendChild(aboutSubDiv2);
+
+  contentDiv.appendChild(mainDiv);
+  mainDiv.appendChild(titleDiv);
+  mainDiv.appendChild(aboutDiv);
+  console.log("hello")
+}
+
+makeTab1();
