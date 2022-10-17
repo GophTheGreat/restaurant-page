61dcd564b7bdc0cf5b263fd9b337a9e9565b077d
Goph <tealeverywhere@gmail.com>
2022-10-16 19:35:10 -1000

    tabs work

--- src/index.js
+++ src/index.js
@@ -7,27 +7,10 @@ let tabContainer = document.createElement("div");
 tabContainer.className = 'tabContainer';
 contentDiv.appendChild(tabContainer);
 
-
-
-//make tabs
-for(let i = 0; i < 3; i++){
-  let tab = document.createElement("div");
-  tab.innerText = `tab${i}`;
-  tabContainer.appendChild(tab);
-}
-
-
 //make tab1
 function makeTab1(){
-  let mainDiv = document.createElement("div");
-  mainDiv.id = "mainDiv";
-  mainDiv.style.gridRowStart = '2';
-  mainDiv.style.gridColumn= '1/4';
-  mainDiv.style.border = "2px solid black";
-  mainDiv.style.display = "grid";
-  mainDiv.style.width = '65%';
-  mainDiv.style.justifyItems = "center";
-  mainDiv.style.gridTemplateRows = "1fr 2fr 2fr";
+  clearTab();
+  let mainDiv = makeMainDiv();
 
   let titleDiv = document.createElement("div");
   titleDiv.innerHTML = "Welcome to Food";
@@ -55,6 +38,56 @@ function makeTab1(){
 }
 
 function makeTab2(){
+  let mainDiv = makeMainDiv();
+
+  let menuDiv1 = document.createElement("div");
+  menuDiv1.innerHTML = "Welcome to Food";
+  menuDiv1.className = "menuDiv1 ";
+  menuDiv1.style.fontSize = "30px";
+  menuDiv1.style.fontFamily = "sans-serif";
+
+  contentDiv.appendChild(mainDiv);
+  mainDiv.appendChild(menuDiv1);
+
+}
+
+function makeTab3(){
+  clearTab();
+  let mainDiv = makeMainDiv();
+
+  let titleDiv = document.createElement("div");
+  titleDiv.innerHTML = "Contact Us";
+  titleDiv.className = "titleDiv";
+  titleDiv.style.fontSize = "30px";
+  titleDiv.style.fontFamily = "sans-serif";
+
+  let contactDiv1 = document.createElement("div");
+  contactDiv1.innerHTML = "Mister Chef"
+  contactDiv1.style.fontSize = "30px";
+  contactDiv1.style.fontFamily = "sans-serif";
+
+  let p = document.createElement('p');
+  p.className = "chef";
+  p.innerHTML = "Chef";
+  contactDiv1.appendChild(p);
+
+  p = document.createElement('p');
+  p.className = "phone";
+  p.innerHTML = "800-867-5309";
+  contactDiv1.appendChild(p);
+
+  p = document.createElement('p');
+  p.className = "email";
+  p.innerHTML = "misterRealEmail@email.org";
+  contactDiv1.appendChild(p);
+
+  contentDiv.appendChild(mainDiv);
+  mainDiv.appendChild(titleDiv);
+  mainDiv.appendChild(contactDiv1);
+
+}
+
+function makeMainDiv(){
   let mainDiv = document.createElement("div");
   mainDiv.id = "mainDiv";
   mainDiv.style.gridRowStart = '2';
@@ -65,13 +98,26 @@ function makeTab2(){
   mainDiv.style.justifyItems = "center";
   mainDiv.style.gridTemplateRows = "1fr 2fr 2fr";
 
-  let menuDiv1 = document.createElement("div");
-  menuDiv1.innerHTML = "Welcome to Food";
-  menuDiv1.className = "menuDiv1 ";
-  menuDiv1.style.fontSize = "30px";
-  menuDiv1.style.fontFamily = "sans-serif";
-
-  mainDiv.appendChild(menuDiv1);
-
+  return mainDiv;
 }
-makeTab1();
+
+
+
+function clearTab(){
+  while(contentDiv.childNodes.length > 1){
+    contentDiv.removeChild(contentDiv.lastChild);
+  }
+}
+
+//make tabs
+let makeTabFunctionArray = [makeTab1, makeTab2, makeTab3];
+for(let i = 0; i < 3; i++){
+  let tab = document.createElement("div");
+  tab.innerText = `tab${i}`;
+  tabContainer.appendChild(tab);
+
+  tab.addEventListener("click", makeTabFunctionArray[i]);
+}
+
+
+makeTab3();
