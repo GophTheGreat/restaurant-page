d2fd15521c07573de008d358bfa4bda8061ece67
Goph <tealeverywhere@gmail.com>
2022-10-27 19:12:02 -1000

    third menu item and formatting improvements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import foodimg1 from './../assets/Food1.jpg'
 import foodimg2 from './../assets/Food2.jpg'
+import foodimg3 from './../assets/Food3.jpg'
+import foodimg4 from './../assets/Food4.jpg'
 
 let contentDiv = document.getElementById("content");
 document.body.style.backgroundImage="url('../assets/GophBGOnly2x.png')";
@@ -114,7 +116,7 @@ function makeTab2(){
   itemDiv1.appendChild(itemDiv1Sub3);
   mainDiv.appendChild(itemDiv1Outer);
 
-  ///////
+  ///////Second Item
 
   let itemDiv2Outer = document.createElement("div");
   itemDiv2Outer.className = "outer";
@@ -143,7 +145,34 @@ function makeTab2(){
   itemDiv2.appendChild(itemDiv2Sub3);
   mainDiv.appendChild(itemDiv2Outer);
 
-  //////
+  ///////Third Item
+  let itemDiv3Outer = document.createElement("div");
+  itemDiv3Outer.className = "outer";
+
+  let itemDiv3 = document.createElement("div");
+  itemDiv3.classList = "inner menu";
+
+  let itemDiv3Sub1 = document.createElement("div");
+  itemDiv3Sub1.classList = "menusub1";
+  itemDiv3Sub1.innerHTML = "Food?"
+
+  let itemDiv3Sub2 = document.createElement("div");
+  itemDiv3Sub2.classList = "menusub2";
+  itemDiv3Sub2.innerHTML = "Some people believe that whatever this is, is edible. Do you dare?";
+
+  let itemDiv3Sub3 = document.createElement("div");
+  itemDiv3Sub3.classList = "menusub3"
+  let food3 = document.createElement("img");
+  food3.className = "foodpic";
+  food3.src = foodimg3
+  itemDiv3Sub3.appendChild(food3);
+
+  itemDiv3Outer.appendChild(itemDiv3);
+  itemDiv3.appendChild(itemDiv3Sub1);
+  itemDiv3.appendChild(itemDiv3Sub2);
+  itemDiv3.appendChild(itemDiv3Sub3);
+  mainDiv.appendChild(itemDiv3Outer);
+
 
 }
 
@@ -190,10 +219,12 @@ function makeMainDiv(){
   mainDiv.style.gridColumn= '1/4';
   mainDiv.style.border = "2px solid black";
   mainDiv.style.display = "grid";
-  mainDiv.style.width = '65%';
+  mainDiv.style.width = '1000px';
   mainDiv.style.justifyItems = "center";
-  mainDiv.style.gridTemplateRows = "5px 2fr 2fr";
+  mainDiv.style.gridTemplateRows = "35px repeat(auto-fit, minmax(400px, 1fr))";
   mainDiv.style.rowGap = "5em";
+  mainDiv.style.paddingBottom = "10px";
+  mainDiv.style.paddingTop = "10px";
 
   return mainDiv;
 }
