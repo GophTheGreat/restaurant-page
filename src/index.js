d32924012c24342a275b326e1d66c3512419e6db
Goph <tealeverywhere@gmail.com>
2022-11-01 20:01:14 -1000

    added hours box, consolidated css

--- src/index.js
+++ src/index.js
@@ -15,58 +15,108 @@ contentDiv.appendChild(tabContainer);
 function makeTab1(){
   clearTab();
   let mainDiv = makeMainDiv();
+  contentDiv.appendChild(mainDiv);
+
+  /////////Title Div
 
   let titleDiv = document.createElement("div");
   titleDiv.innerHTML = "Welcome to Food";
   titleDiv.className = "titleDiv";
   titleDiv.style.fontSize = "30px";
   titleDiv.style.fontFamily = "sans-serif";
+  mainDiv.appendChild(titleDiv);
 
-  let aboutDiv = document.createElement("div")
-  aboutDiv.style.minWidth = "450px";
-  aboutDiv.style.maxWidth = "80%";
-  aboutDiv.style.backgroundColor = "rgb(29, 249, 189)";
-  aboutDiv.style.clipPath = " polygon(100% 0, 100% 89%, 50% 100%, 0 89%, 0 0)";
-  aboutDiv.style.display = "grid";
-  aboutDiv.style.gridTemplateRows = "1fr 3fr 1fr";
+  ////////About Div
+
+  let aboutDiv = document.createElement("div");
+  aboutDiv.className = "outer";
+  // aboutDiv.style.minWidth = "450px";
+  // aboutDiv.style.maxWidth = "80%";
+  // aboutDiv.style.backgroundColor = "rgb(29, 249, 189)";
+  // aboutDiv.style.clipPath = " polygon(100% 0, 100% 89%, 50% 100%, 0 89%, 0 0)";
+  // aboutDiv.style.display = "grid";
+  // aboutDiv.style.gridTemplateRows = "1fr 3fr 1fr";
+
+  let aboutDivInner = document.createElement("div");
+  aboutDivInner.className = "inner";
+
+  aboutDiv.appendChild(aboutDivInner);
 
   let aboutSubDiv1 = document.createElement("div")
   aboutSubDiv1.innerHTML = "About This Restaurant"
-  aboutSubDiv1.style.display = "grid";
-  aboutSubDiv1.style.justifySelf = "center";
-  aboutSubDiv1.style.textAlign = "center";
-  aboutSubDiv1.style.fontFamily = "sans-serif";
+  aboutSubDiv1.className = "menusub1";
+  // aboutSubDiv1.style.display = "grid";
+  // aboutSubDiv1.style.justifySelf = "center";
+  // aboutSubDiv1.style.textAlign = "center";
+  // aboutSubDiv1.style.fontFamily = "sans-serif";
   aboutSubDiv1.style.fontSize = "40px";
-  aboutSubDiv1.style.fontWeight = "bold";
-  aboutSubDiv1.style.overflowWrap = "normal";
-  aboutSubDiv1.style.width = "60%";
+  // aboutSubDiv1.style.fontWeight = "bold";
+  // aboutSubDiv1.style.overflowWrap = "normal";
+  // aboutSubDiv1.style.width = "60%";
 
-  aboutDiv.appendChild(aboutSubDiv1);
+  aboutDivInner.appendChild(aboutSubDiv1);
 
   let aboutSubDiv2 = document.createElement("div");
-  aboutSubDiv2.innerHTML = "“Look, having nuclear—my uncle was a great professor and scientist and engineer, Dr. John Trump at MIT; good genes, very good genes, OK, very smart, the Wharton School of Finance, very good, very smart —you know, if you’re a conservative Republican, if I were a liberal, if, like, OK, if I ran as a liberal Democrat, they would say I’m one of the smartest people anywhere in the world—it’s true!—but when you’re a conservative Republican they try—oh, do they do a number—that’s why I always start off: Went to Wharton, was a good student, went there, went there, did this, built a fortune—you know I have to give my like credentials all the time, because we’re a little disadvantaged—but you look at the nuclear deal, the thing that really bothers me—it would have been so easy, and it’s not as important as these lives are (nuclear is powerful; my uncle explained that to me many, many years ago, the power and that was 35 years ago; he would explain the power of what’s going to happen and he was right—who would have thought?), but when you look at what’s going on with the four prisoners—now it used to be three, now it’s four—but when it was three and even now, I would have said it’s all in the messenger; fellas, and it is fellas because, you know, they don’t, they haven’t figured that the women are smarter right now than the men, so, you know, it’s gonna take them about another 150 years—but the Persians are great negotiators, the Iranians are great negotiators, so, and they, they just killed, they just killed us."
+  aboutSubDiv2.innerHTML = `“Look, having nuclear—my uncle was a great professor and scientist and engineer, Dr. John Trump at MIT; good genes, very good genes, OK, very smart, the Wharton School of Finance, very good, very smart —you know, if you’re a conservative Republican, if I were a liberal, if, like, OK, if I ran as a liberal Democrat, they would say I’m one of the smartest people anywhere in the world—it’s true!—but when you’re a conservative Republican they try—oh, do they do a number—that’s why I always start off..."`
   aboutSubDiv2.style.fontSize = "19px";
   aboutSubDiv2.style.margin = "15px";
   
-  aboutDiv.appendChild(aboutSubDiv2);
+  aboutDivInner.appendChild(aboutSubDiv2);
   
   let aboutSubDiv3 = document.createElement("div");
   aboutSubDiv3.innerHTML = "Chef \"The Best\" Marioluigi";
+  aboutSubDiv3.className = "menusub1";
   aboutSubDiv3.style.fontSize = "24px";
   aboutSubDiv3.style.fontWeight = "bold"
-  aboutSubDiv3.style.fontFamily = "sans-serif";
-  aboutSubDiv3.style.display = "grid";
-  aboutSubDiv3.style.justifySelf = "center";
-  aboutSubDiv3.style.alignSelf = "end";
-  aboutSubDiv3.style.marginTop = "45px";
-  aboutSubDiv3.style.marginBottom = "45px";
-  aboutDiv.appendChild(aboutSubDiv3);
+  aboutDivInner.appendChild(aboutSubDiv3);
 
-
-
-  contentDiv.appendChild(mainDiv);
-  mainDiv.appendChild(titleDiv);
   mainDiv.appendChild(aboutDiv);
+
+  ////////Hour div
+
+  let hourDiv = document.createElement("div");
+  hourDiv.className = "outer"
+  hourDiv.style.height = '265px';
+  hourDiv.style.marginTop = '35px';
+  mainDiv.appendChild(hourDiv);
+  let hourDivInner = document.createElement("div");
+  hourDivInner.className = "inner"
+  hourDivInner.display = "grid";
+  hourDivInner.style.gridTemplateRows = '55px repeat(5, 35px)';
+  hourDivInner.style.height = '255px';
+  hourDiv.appendChild(hourDivInner);
+
+  let hourDivSub1 = document.createElement("div")
+  hourDivSub1.innerHTML = "Hours"
+  hourDivSub1.className = "menusub1";
+  hourDivSub1.style.fontSize = "40px";
+  hourDivInner.appendChild(hourDivSub1);
+
+  let hourDivSub2 = document.createElement("div")
+  hourDivSub2.innerHTML = "Mon: 0100 - 2300"
+  hourDivSub2.className = "hours";
+  hourDivInner.appendChild(hourDivSub2);
+
+  let hourDivSub3 = document.createElement("div")
+  hourDivSub3.innerHTML = "Tue: 0100 - 2300"
+  hourDivSub3.className = "hours";
+  hourDivInner.appendChild(hourDivSub3);
+
+  let hourDivSub4 = document.createElement("div")
+  hourDivSub4.innerHTML = "Wed: 0100 - 2300";
+  hourDivSub4.className = "hours";
+  hourDivInner.appendChild(hourDivSub4);
+
+  let hourDivSub5 = document.createElement("div")
+  hourDivSub5.innerHTML = "Thu: 0100 - 0200";
+  hourDivSub5.className = "hours";
+  hourDivInner.appendChild(hourDivSub5);
+
+  let hourDivSub6 = document.createElement("div")
+  hourDivSub6.innerHTML = "Fri: Closed";
+  hourDivSub6.className = "hours";
+  hourDivInner.appendChild(hourDivSub6);
+
   console.log("hello")
 }
 
@@ -76,7 +126,7 @@ function makeTab2(){
 
   contentDiv.appendChild(mainDiv);
 
-  ///////
+  ///////Header
 
   let menuDiv1 = document.createElement("div");
   menuDiv1.innerHTML = "Menu";
@@ -86,7 +136,7 @@ function makeTab2(){
 
   mainDiv.appendChild(menuDiv1);
 
-  ///////
+  ///////First Item
 
   let itemDiv1Outer = document.createElement("div");
   itemDiv1Outer.className = "outer";
@@ -248,4 +298,4 @@ for(let i = 0; i < 3; i++){
 }
 
 
-makeTab2();
+makeTab1();
