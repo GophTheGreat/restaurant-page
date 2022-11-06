6bdadf50aaf0720019f0d2f3327a541e16b58b6a
Goph <tealeverywhere@gmail.com>
2022-11-05 18:16:16 -1000

    finalize contact page

--- src/index.js
+++ src/index.js
@@ -243,33 +243,60 @@ function makeTab3(){
 
   let titleDiv = document.createElement("div");
   titleDiv.innerHTML = "Contact Us";
-  titleDiv.className = "titleDiv";
-  titleDiv.style.fontSize = "30px";
-  titleDiv.style.fontFamily = "sans-serif";
-
-  let contactDiv1 = document.createElement("div");
-  contactDiv1.innerHTML = "Mister Chef"
-  contactDiv1.style.fontSize = "30px";
-  contactDiv1.style.fontFamily = "sans-serif";
-
-  let p = document.createElement('p');
-  p.className = "chef";
-  p.innerHTML = "Chef";
-  contactDiv1.appendChild(p);
-
-  p = document.createElement('p');
-  p.className = "phone";
-  p.innerHTML = "800-867-5309";
-  contactDiv1.appendChild(p);
-
-  p = document.createElement('p');
-  p.className = "email";
-  p.innerHTML = "misterRealEmail@email.org";
-  contactDiv1.appendChild(p);
-
-  contentDiv.appendChild(mainDiv);
+  titleDiv.className = "header";
   mainDiv.appendChild(titleDiv);
-  mainDiv.appendChild(contactDiv1);
+
+
+
+  let contactDiv1Outer = document.createElement("div");
+  contactDiv1Outer.classList = "outer contactOuter";
+  let contactDiv1 = document.createElement("div");
+  contactDiv1.classList = "inner contactInner";
+
+  let contactDiv1Sub1 = document.createElement("div");
+  contactDiv1Sub1.className = "menusub1"
+  contactDiv1Sub1.innerHTML = `Chef "The Best" Marioluigi`
+  contactDiv1Sub1.style.fontSize = "30px";
+  contactDiv1Sub1.style.fontFamily = "sans-serif";
+
+  let contactDiv1Sub2 = document.createElement("div");
+  contactDiv1Sub2.className = "menusub1"
+  contactDiv1Sub2.innerHTML = `Chef`
+
+  let contactDiv1Sub3 = document.createElement("div");
+  contactDiv1Sub3.className = "menusub1"
+  contactDiv1Sub3.innerHTML = `1-800-867-5309`
+
+  let contactDiv1Sub4 = document.createElement("div");
+  contactDiv1Sub4.className = "menusub1"
+  contactDiv1Sub4.innerHTML = `misterRealEmail@email.org`
+
+  contactDiv1Outer.appendChild(contactDiv1);
+  contactDiv1.appendChild(contactDiv1Sub1);
+  contactDiv1.appendChild(contactDiv1Sub2);
+  contactDiv1.appendChild(contactDiv1Sub3);
+  contactDiv1.appendChild(contactDiv1Sub4);
+  mainDiv.appendChild(contactDiv1Outer);
+
+  /////////
+
+  let happyface = document.createElement("div");
+  happyface.innerHTML = `:)`;
+  happyface.style.fontSize = "470px";
+  happyface.style.fontFamily = "sans-serif";
+  happyface.style.color = "gold";
+  happyface.style.display = "grid";
+  happyface.style.margin = "-250px"
+  happyface.style.transform = "rotate(90deg)"
+  happyface.style.textAlign = "center";
+  happyface.style.alignContent = "center";
+  happyface.style.paddingBottom = "84px";
+  happyface.style.height = "550px";
+  happyface.style.width = "550px";
+
+  mainDiv.appendChild(happyface);
+  contentDiv.appendChild(mainDiv);
+
 
 }
 
@@ -317,4 +344,4 @@ tab.innerHTML = "Contact";
 
 
 
-makeTab2();
+makeTab3();
